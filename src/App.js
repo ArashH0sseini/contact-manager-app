7bacbd69566eda0678257821da9880dafcd17863
Arash Hosseini <arashhosseini733@gmail.com>
2022-07-15 18:58:32 +0430

    make navbar components and add colors to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-     <h3>اپلیکیشن مدیریت مخاطبین</h3>
-     <button className='btn btn-primary'>button</button>
-     <span className='fa fa-user'></span>
+     <Navbar />
     </div>
   );
 }
