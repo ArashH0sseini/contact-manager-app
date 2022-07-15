41596c977629b55f36bfde13a34f38165a196f73
Arash Hosseini <arashhosseini733@gmail.com>
2022-07-15 20:04:14 +0430

    create contacts and contact components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import './App.css';
+import Contacts from './components/contacts/contacts';
 
 const App = () => {
   return (
     <div className="App">
      <Navbar />
+     <Contacts />
     </div>
   );
 }
