90ab35e931348065b531e9955f24daa355cd9de0
Arash Hosseini <arashhosseini733@gmail.com>
2022-07-15 17:34:12 +0430

    add bootstrap , font awsome and custom font

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
      <h3>اپلیکیشن مدیریت مخاطبین</h3>
+     <button className='btn btn-primary'>button</button>
+     <span className='fa fa-user'></span>
     </div>
   );
 }
